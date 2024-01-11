97a83319f2399db3cbb0c593970edf52d0854206
ahmadoa <ouladaouida@gmail.com>
2024-01-11 21:27:50 +0000

    updated redactingFormatter function according to requirements & added get_logger function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -21,3 +21,38 @@ def filter_datum(
     """returns log message obfsucated"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, seperator), replace(redaction), message)
+
+
+def get_logger() -> logging.Logger:
+    """creates new logger for user data"""
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """formats a logRecord"""
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPERATOR)
+        return txt
+
+
+if __name__ == "__main__":
+    main()
