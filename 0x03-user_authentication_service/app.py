e78f8a18c4f537ebf413495a55c203a7b302952a
ahmadoa <ouladaouida@gmail.com>
2024-01-24 01:44:34 +0000

    implemented an end-point to register new user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -19,5 +19,30 @@ def status() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def new_user() -> str:
+    """ POST /users
+    Registers new user with email and pswd in request form-data,
+    or finds if user already registered based on email
+    Return:
+      - JSON payload
+    """
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        new_user = AUTH.register_user(email, password)
+        if new_user is not None:
+            return jsonify({
+                "email": new_user.email,
+                "message": "user created"
+            })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+        }), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
