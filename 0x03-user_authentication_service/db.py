b70311aa621f3525e8bf250e8f63435b39456e22
ahmadoa <ouladaouida@gmail.com>
2024-01-24 00:57:42 +0000

    updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,12 +8,7 @@ from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-from user import Base
-from user import User
-
-from typing import (
-    Dict, Any
-)
+from user import Base, User
 
 
 class DB:
@@ -23,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -44,12 +39,12 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs: Dict[str, Any]) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """Query database to find user using kwargs"""
-        for k in kwargs.keys():
-            if not hasattr(User, k):
-                raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
+        try:
+            record = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if record is None:
             raise NoResultFound
-        return user
+        return record
