8ab32fbb66be4214bbeb3cead1e8331825d4e599
ahmadoa <ouladaouida@gmail.com>
2024-01-24 01:35:49 +0000

    created a flask app and added get route /

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,23 @@
+#!/#!/usr/bin/env python3
+""" Route module for the API """
+
+from flask import Flask, jsonify, request,  abort, redirect, url_for
+from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
+
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /status
+    Return:
+      - JSON payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
