f568f0960d6e428ead1704aa12408e7b690e0f0a
ahmadoa <ouladaouida@gmail.com>
2024-01-26 02:12:57 +0000

    implemented profile function that responds with a json payload in case a user was found

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -80,5 +80,21 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ GET /profile
+    Return:
+        - Use session_id to find the user.
+        - 403 if session ID is invalid
+    """
+    user_cookie = request.cookies.get("session_id", None)
+    if user_cookie is None:
+        abort(403)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
